38f88d2d79674e259aec405ee8af532cabcd903c
Douglas Ramírez <ra.douglas@hotmail.com>
2023-04-13 16:34:57 -0500

    Basic UserController scaffold

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,5 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware('auth:api')->group(function() {
     // Protected routes
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/me', [UserController::class, 'me'])->name('me');
 });
