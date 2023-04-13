2c43773c6a2e13cb8ed970a59f96797de450f9ee
Douglas Ramírez <ra.douglas@hotmail.com>
2023-04-13 14:55:24 -0500

    Base log in scaffold

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(LoginRequest $request) 
+    {
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return true;
 });
+
+Route::post('/login', [LoginController::class, 'login'])->name('login');
