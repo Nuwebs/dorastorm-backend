96b5f15e4c84c2f553495c4d95898e65662a15f3
Douglas Ramírez <ra.douglas@hotmail.com>
2023-04-13 12:10:48 -0500

    Public folder rename method

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
+$app->usePublicPath($app->basePath('public'));
 
 /*
 |--------------------------------------------------------------------------
