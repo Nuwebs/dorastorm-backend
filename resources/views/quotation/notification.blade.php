93f0b9ef17065f200402cc8332da410d6f366799
Douglas Ramírez <ra.douglas@hotmail.com>
2023-04-16 17:42:53 -0500

    Quotation emails blade templates

---
+++ resources/views/quotation/notification.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@lang('emails.quotations.notification.title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container d-flex justify-content-center">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">@lang('emails.quotations.notification.content_title')</h5>
+                <p class="card-text">@lang('emails.quotations.notification.content')</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
