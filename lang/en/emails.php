b30a64888384cd6d3f2de0550bd771fc6eb224a2
Douglas Ramírez <ra.douglas@hotmail.com>
2023-04-16 17:33:46 -0500

    (MINOR) Localization lang packages

---
+++ lang/en/emails.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'general_subject' => 'We received your message',
+    'quotations' =>  [
+        'title' => 'Quotation confirmation',
+        'content_title' => 'Hello',
+        'content' => 'We received your quotation. We are going to respond you as soon as possible.',
+        'notification' => [
+            'title' => 'Quotation received',
+            'content_title' => 'Quotation received',
+            'content' => 'You can check it in the information system.'
+        ]
+    ]
+];
---
+++ lang/es/emails.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'general_subject' => 'Recibimos tu mensaje',
+    'quotations' =>  [
+        'title' => 'Confirmación de cotización',
+        'content_title' => 'Hola',
+        'content' => 'Recibimos tu solicitud de cotización. Responderemos tan pronto como podamos.',
+        'notification' => [
+            'title' => 'Cotización recibida',
+            'content_title' => 'Se ha recibido una cotización',
+            'content' => 'Puede verla ingresando al sistema de información.'
+        ]
+    ]
+];
