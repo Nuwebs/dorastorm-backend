49b3d9fb7301cffd71f49a069c019de9eaf73f0c
Douglas Ramírez <ra.douglas@hotmail.com>
2023-04-22 17:24:18 -0500

    Default CORS config changed

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
 
     'allowed_origins_patterns' => [],
 
