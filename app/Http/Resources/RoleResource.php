5e8259937d5dd2de21a3003c61ea573147d482e0
Douglas Ramírez <ra.douglas@hotmail.com>
2023-12-20 05:26:53 -0500

    Removed unused migrations

--- app/Http/Resources/RoleResource.php
+++ app/Http/Resources/RoleResource.php
@@ -24,8 +24,8 @@ class RoleResource extends JsonResource
             'name' => $this->display_name,
             'description' => $this->description,
             'permissions' => $rolePermissions,
-            'created' => $this->created_at,
-            'modified' => $this->updated_at,
+            'created_at' => $this->created_at,
+            'modified_at' => $this->updated_at,
         ];
     }
 }
